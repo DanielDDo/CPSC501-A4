856b53d48121881b3e52d8027b12d337232f739a
dudo <dudo@zone07-wa.cs.ucalgary.ca>
2017-12-01 15:50:54 -0700

    Generalized the fileparser to work with any .wav file.

--- convolution.cpp
+++ convolution.cpp
@@ -23,70 +23,80 @@ struct WAV{
   vector<unsigned char> buffer4(4);
   vector<unsigned char> buffer2(2);
 
-int main(int argc, char *argv[]) {
-  struct WAV wav;
-  // open a file
+void parseFile(char *fileName, struct WAV *infoHolder) {
   ifstream file;
-  file.open(argv[1], ios::out | ios::binary);
+  file.open(fileName, ios::out | ios::binary);
+
   if (file.is_open()) {
     // Read chunkID - big endian
     file.read((char*) &buffer4[0], buffer4.size());
-    copy(buffer4.begin(), buffer4.end(), wav.chunkID);
+    copy(buffer4.begin(), buffer4.end(), infoHolder->chunkID);
 
     // Read chunkSize - little endian
     file.read((char*) &buffer4[0], buffer4.size());
-    wav.chunkSize = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
+    infoHolder->chunkSize = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
 
     // Read format - big endian
     file.read((char*) &buffer4[0], buffer4.size());
-    copy(buffer4.begin(), buffer4.end(), wav.format);
+    copy(buffer4.begin(), buffer4.end(), infoHolder->format);
 
     // Read subChunk1ID - big endian
     file.read((char*) &buffer4[0], buffer4.size());
-    copy(buffer4.begin(), buffer4.end(), wav.subChunk1ID);
+    copy(buffer4.begin(), buffer4.end(), infoHolder->subChunk1ID);
 
     // Read subChunk1Size - little endian
     file.read((char*) &buffer4[0], buffer4.size());
-    wav.subChunk1Size = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
+    infoHolder->subChunk1Size = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
 
     // Read audioFormat - little endian
     file.read((char*) &buffer2[0], buffer2.size());
-    wav.audioFormat = (buffer2[0] << 0) | (buffer2[1] << 8);
+    infoHolder->audioFormat = (buffer2[0] << 0) | (buffer2[1] << 8);
 
     // Read numChannels - little endian
     file.read((char*) &buffer2[0], buffer2.size());
-    wav.numChannels = (buffer2[0] << 0) | (buffer2[1] << 8);
+    infoHolder->numChannels = (buffer2[0] << 0) | (buffer2[1] << 8);
 
     // Read sampleRate - little endian
     file.read((char*) &buffer4[0], buffer4.size());
-    wav.sampleRate = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
+    infoHolder->sampleRate = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
 
     // Read byteRate - little endian
     file.read((char*) &buffer4[0], buffer4.size());
-    wav.byteRate = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
+    infoHolder->byteRate = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
 
     // Read blockAlign - little endian
     file.read((char*) &buffer2[0], buffer2.size());
-    wav.blockAlign = (buffer2[0] << 0) | (buffer2[1] << 8);
+    infoHolder->blockAlign = (buffer2[0] << 0) | (buffer2[1] << 8);
 
     // Read bitsPerSample - little endian
     file.read((char*) &buffer2[0], buffer2.size());
-    wav.bitsPerSample = (buffer2[0] << 0) | (buffer2[1] << 8);
+    infoHolder->bitsPerSample = (buffer2[0] << 0) | (buffer2[1] << 8);
 
     // discard extra bits if they're present
-    if (wav.subChunk1Size > 16) {
-      file.read((char*) &buffer4[0], wav.subChunk1Size-16);
+    if (infoHolder->subChunk1Size > 16) {
+      file.read((char*) &buffer4[0], infoHolder->subChunk1Size-16);
     }
 
     // Read subChunk2ID - big endian
     file.read((char*) &buffer4[0], buffer4.size());
-    copy(buffer4.begin(), buffer4.end(), wav.subChunk2ID);
+    copy(buffer4.begin(), buffer4.end(), infoHolder->subChunk2ID);
 
     // Read subChunk2Size - little endian
     file.read((char*) &buffer4[0], buffer4.size());
-    wav.subChunk2Size = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
+    infoHolder->subChunk2Size = (buffer4[0] << 0) | (buffer4[1] << 8) | (buffer4[2] << 16) | (buffer4[3] << 24);
   }
   file.close();
+}
+
+
+int main(int argc, char *argv[]) {
+  struct WAV wav;
+  struct WAV ir;
+  // open a file
+  parseFile(argv[1], &wav);
+
+  parseFile(argv[2], &ir);
+
 
   return 0;
 }
